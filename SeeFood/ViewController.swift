dac9ebf473769ca06af2d022e1659c588339f823
paul thi <paulthi@pauls-macbook-pro.local>
2019-02-16 10:26:56 -0500

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by paul thi on 2/16/19.
+//  Copyright © 2019 Ambam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
